07fe7f19c1328ed91904d8629eafed4fdc8b1480
John <jdpivotjohn@gmail.com>
2018-05-01 04:07:13 -0500

    about section columns align and different line heights about ready

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,3 +1,23 @@
-$('document').ready(function(){
-  alert('this is working');
-})
+import $ from 'jquery';
+
+class MobileMenu{
+
+  constructor(){
+
+    this.menuIcon = $(".site-header__menu-icon");
+    this.events();
+  }
+
+  events(){
+    this.menuIcon.click(this.toggleTheMenu);
+  }
+
+  toggleTheMenu(){
+    alert("this is working");
+  }
+}
+
+
+
+
+export default MobileMenu;
