cea7c997115243c672d97285b34c2a57bf4722ad
John <jdpivotjohn@gmail.com>
2018-05-31 04:51:39 -0500

    general layout done, work on for next

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,2 @@
 import $ from 'jquery';
-
-//An example on how to add an object and implemet it
-// var mobileMenu = new MobileMenu();
-// new RevealOnScroll($(".feature-item"), "85%");
+import Navigation from './modules/Navigation';
